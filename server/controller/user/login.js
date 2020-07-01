1482c6e219bbc7ceaa663522981cd10238f25ccb
aiora32 <uukel32@gmail.com>
2020-07-02 00:10:52 +0900

    feat login

--- server/controller/user/login.js
+++ server/controller/user/login.js
@@ -1,5 +1,31 @@
 const { users } = require('../../models');
-const crypto = require('crypto');
+//const crypto = require('crypto');
 module.exports = {
-  post: (req, res) => {},
+  post: (req, res) => {
+    let { email, password } = req.body;
+    var sess = req.session;
+
+    users
+    .findOne({
+      where: {
+        email: email,
+        password: password,
+      },
+    })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: "ID and password do not match"
+        });
+      } else {
+        sess.userid = data.id;
+        res.status(200).json({
+          id: data.id,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+  },
 };
