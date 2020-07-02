2cc53149c51e2f38cce9de18ff2a76fbe720f51d
Daseul Sim <tlaektmf27@gmail.com>
2020-07-02 11:44:22 +0900

    Feat goal API

--- server/controller/mypage/goal.js
+++ server/controller/mypage/goal.js
@@ -1,5 +1,25 @@
 const { todoboxes } = require('../../models');
-
+const { users } = require('../../models');
+function algorithm() {}
 module.exports = {
-  post: (req, res) => {},
+  post: (req, res) => {
+    let { startDate, id, weekly, keyword, deadLine } = req.body;
+    users
+      .update(
+        {
+          keyword: keyword,
+        },
+        {
+          where: {
+            id: id,
+          },
+        },
+      )
+      .catch((err) => {
+        res.status(404).send("Dont't find userID");
+      });
+
+    let arr = algorithm(stratDate, weekly, deadLine);
+    res.send(arr);
+  },
 };
