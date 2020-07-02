f15176c69f6b17adea0f008af189cc20808065eb
choisungyu <maroon92@naver.com>
2020-07-02 09:30:32 +0900

    feat router link login component

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <Switch>
-            <Route path="/login" component={Login} />
+            <Route exact path="/login" component={Login} />
             <Route path="/mypage" component={Mypage} />
           </Switch>
         </BrowserRouter>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import {
   Button,
   Form,
@@ -31,9 +31,7 @@ class Login extends Component {
           <span className="font-weight-bold">Habitube</span>
         </h1>
         <FormGroup>
-          <Label mt-5 size="lg">
-            Email
-          </Label>
+          <Label>Email</Label>
           <Input invalid type="email" placeholder="Email" />
           <FormFeedback>이미 등록된 이메일에요!!</FormFeedback>
           <FormText>이메일을 입력하세요.</FormText>
@@ -44,25 +42,26 @@ class Login extends Component {
           </FormGroup>
         </FormGroup>
         <FormGroup>
-          <Label size="lg">Password</Label>
+          <Label>Password</Label>
           <Input invalid type="password" placeholder="Password" />
           <FormFeedback>숫자로 이루어진 비밀번호는 없습니다.</FormFeedback>
           <FormText>비밀번호를 입력하세요.</FormText>
           <br></br>
-          <br></br>
         </FormGroup>
-        <ButtonToggle className="btn-lg btn-block" color="primary">
-          로그인
-        </ButtonToggle>{' '}
-        <ButtonToggle className="btn-lg btn-block" color="secondary">
-          회원가입
-        </ButtonToggle>{' '}
+        <Link to="/mypage">
+          <ButtonToggle className="btn-lg btn-block" color="primary">
+            로그인
+          </ButtonToggle>{' '}
+        </Link>
+        <Link to="/signup">
+          <ButtonToggle className="btn-lg btn-block" color="secondary">
+            회원가입
+          </ButtonToggle>{' '}
+        </Link>
         <div className="text-center pt-3">
           회원가입이 안 되어있으면 회원가입 하세요
         </div>
         <div className="text-center">
-          <a href="/signup">회원가입</a>
-          <span className="p-2">|</span>
           <a href="/forgot-password">비밀번호를 잊어버렸어요</a>
         </div>
       </Form>
