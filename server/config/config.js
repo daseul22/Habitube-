f551db051597f261b3195d5af31764b973d53f2a
Daseul Sim <tlaektmf27@gmail.com>
2020-07-04 12:05:28 +0900

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'root',
+    password: 'asd1234%',
     database: 'habitube',
-    host: 'localhost',
+    host: 'habitube-first.cherr9llnuvi.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
     logging: false,
   },
